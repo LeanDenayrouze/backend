ff69f97255ef1a55f45fc4fc1c5e609f754ef627
LeanDenayrouze <falec@hotmail.com>
2022-10-31 12:43:59 -0300

    parte 20 30'

--- src/main/java/com/portfolio/LC/Repository/REducacion.java
+++ src/main/java/com/portfolio/LC/Repository/REducacion.java
@@ -9,5 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface REducacion extends JpaRepository<Educacion, Integer>{
     public Optional<Educacion> findByNombreE(String nombreE);
-    public Boolean existsByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
 }
